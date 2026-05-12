243975030222720ccab4dc97632d10bffd98e43e
Baek-jisun <jisen41@gmail.com>
2026-05-13 05:26:05 +0900

    config.py 보완

--- config.py
+++ config.py
@@ -4,12 +4,8 @@ os.environ["ANONYMIZED_TELEMETRY"] = "False"
 
 MODEL_NAME = "hf.co/MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M"
 UPLOAD_DIR = "data"
-SOURCE_DIR = "sources"
 DB_PATH = "chroma_db"
 MAX_STORAGE_MB = 200
 
 if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
-
-if not os.path.exists(SOURCE_DIR):
-    os.makedirs(SOURCE_DIR)
